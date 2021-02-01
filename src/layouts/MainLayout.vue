466b0f510d2eb0227a8aa09e991f2a05e0b02493
Vicellken <gulivecassie@gmail.com>
2021-02-01 11:09:15 +0800

    dev template

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title> Quasar App </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label header class="text-grey-8">
+          Essential Links
+        </q-item-label>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "MainLayout",
+
+  data() {
+    return {
+      leftDrawerOpen: false,
+    };
+  },
+};
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page class="flex flex-center">
+    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
+    <q-btn to="demoMap">click to the demo map</q-btn>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageIndex",
+};
+</script>
