5da0feea368cbade67e7f6c17ba9d6a876017af7
Vicellken <gulivecassie@gmail.com>
2021-02-18 20:08:17 +0800

    change GeoJSON feature color

--- src/pages/iH/interactiveHabitat.vue
+++ src/pages/iH/interactiveHabitat.vue
@@ -4,23 +4,15 @@
       <div
         id="parent"
         class="full-width row inline wrap justify-start items-start content-start"
-        style="overflow: hidden;"
+        style="overflow: hidden"
       >
-        <div
-          id="sidebar"
-          class="col-4 bg-grey-6"
-          style="overflow: auto;"
-        >
-          <q-card
-            dark
-            bordered
-            class="q-py-md bg-blue-grey-13 my-card"
-          >
+        <div id="sidebar" class="col-4 bg-grey-6" style="overflow: auto">
+          <q-card dark bordered class="q-py-md bg-blue-grey-13 my-card">
             <div class="q-pa-md row justify-center">
               <q-img
                 src="~/assets/SWIMSlogo2.png"
                 alt="SWIMs logo"
-                style="width: 120px; height:100%; align: center"
+                style="width: 120px; height: 100%; align: center"
               />
             </div>
 
@@ -28,14 +20,20 @@
               WElCOME PAGE
             </div>
             <div class="text-h6 text-bold text-center text-white">
-              Interactive Habitat Map<br>Around Hong Kong Rocky Shore<br>by ECF 2019-2023
+              Interactive Habitat Map<br />Around Hong Kong Rocky Shore<br />by
+              ECF 2019-2023
             </div>
             <!-- <q-separator
               dark
               inset
             /> -->
             <div class="q-ma-sm">
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quis tempore praesentium illum? Molestiae ipsa repellendus ipsam! Quaerat placeat eveniet, quo tempora dolorum, maxime eos odit, voluptate aspernatur assumenda ipsa.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+                quis tempore praesentium illum? Molestiae ipsa repellendus
+                ipsam! Quaerat placeat eveniet, quo tempora dolorum, maxime eos
+                odit, voluptate aspernatur assumenda ipsa.
+              </p>
             </div>
 
             <div class="row justify-center">
@@ -47,12 +45,9 @@
                 style="width: 280px"
               >
                 <div class="row items-center no-wrap">
-                  <q-icon
-                    center
-                    name="zoom_in"
-                  />
+                  <q-icon center name="zoom_in" />
                   <div class="text-center no-caps">
-                    Please Select Any Zone<br>for Localized Data
+                    Please Select Any Zone<br />for Localized Data
                   </div>
                 </div>
               </q-btn>
@@ -60,10 +55,7 @@
           </q-card>
         </div>
 
-        <div
-          class="col-8 bg-grey-6"
-          style="overflow: auto"
-        >
+        <div class="col-8 bg-grey-6" style="overflow: auto">
           <div class="map">
             <l-map
               style="height: 480px; width: 100%"
@@ -74,33 +66,26 @@
               :max-bounds="maxBounds"
               :max-zoom="maxZoom"
               :min-zoom="minZoom"
+              ref="myMapRef"
             >
-              <l-tile-layer
-                :url="url"
-                :attribution="attribution"
-              />
-              <l-geo-json
-                :geojson="geojson"
-                :color="geojson.color"
-              />
-              </l-geo-json>
+              <l-tile-layer :url="url" :attribution="attribution" />
+              <l-geo-json :geojson="geojson" :options="geoJSONOptions" />
             </l-map>
           </div>
         </div>
       </div>
     </div>
-    <div class="col-6">
-    </div>
+    <div class="col-6"></div>
   </div>
 </template>
 
 <script>
 import "leaflet/dist/leaflet.css";
-import { LMap, LTileLayer, LMarker, LGeoJson } from "vue2-leaflet";
+import { LMap, LTileLayer, LGeoJson } from "vue2-leaflet";
 
 export default {
   name: "MapGeoJSON",
-  components: { LMap, LTileLayer, LGeoJson, LMarker },
+  components: { LMap, LTileLayer, LGeoJson },
   data() {
     return {
       zoom: 10.25,
@@ -123,6 +108,33 @@ export default {
         zoomSnap: 0.25,
       },
       geojson: require("../../MapData/regions.json"),
+
+      geoJSONOptions: {
+        style: function style(feature) {
+          return {
+            // weight: feature.properties.stroke_width,
+            opacity: 0.6,
+            color: feature.properties.fill /* refer to json data */,
+            fillOpacity: 0.3,
+          };
+        },
+        onEachFeature: function onEachFeature(feature, layer) {
+          layer.on("mouseover", (e) => {
+            // console.log(feature.properties.Name);
+            
+            /* cannot open the popup */
+            // //open popup
+            // var popup = L.popup()
+            //   .setLatLng(e.latlng)
+            //   .setContent(
+            //     '<h1 style="color: grey; margin-bottom: 0;">' +
+            //       feature.properties.Name +
+            //       "</h1>"
+            //   )
+            //   .openOn(self.$refs.myMapRef.mapObject);
+          });
+        },
+      },
     };
   },
 };
