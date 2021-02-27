8b570ca6283ea116860188d03c805cd36286ba8a
Vicellken <gulivecassie@gmail.com>
2021-02-27 16:32:11 +0800

    layout update

--- src/pages/iH/interactiveHabitat.vue
+++ src/pages/iH/interactiveHabitat.vue
@@ -1,108 +1,69 @@
 <template>
-  <div class="q-px-xs">
-    <div class="flex flex-center column">
-      <div
-        id="parent"
-        class="full-width row inline wrap justify-start items-start content-start"
-        style="overflow: hidden"
-      >
-        <div
-          id="sidebar"
-          class="col-4 bg-grey-6"
-          style="overflow: auto"
-        >
-          <q-card
-            dark
-            bordered
-            class="q-py-md bg-blue-grey-13 my-card"
-          >
-            <div class="q-pa-md row justify-center">
-              <q-img
-                src="~/assets/SWIMSlogo2.png"
-                alt="SWIMs logo"
-                style="width: 120px; height: 100%; align: center"
-              />
-            </div>
+  <div class="row q-pa-sm">
+    <div id="sidebar" class="col-4" style="overflow: auto">
+      <q-card dark bordered class="q-py-md bg-blue-grey-13 my-card">
+        <div class="q-pa-md row justify-center">
+          <q-img
+            src="~/assets/SWIMSlogo2.png"
+            alt="SWIMs logo"
+            style="width: 120px; height: 100%; align: center"
+          />
+        </div>
 
-            <div class="bg-blue-10 text-h6 text-bold text-center text-white">
-              WElCOME PAGE
-            </div>
-            <div class="text-h6 text-bold text-center text-white">
-              Interactive Habitat Map<br />Around Hong Kong Rocky Shore<br />by
-              ECF 2019-2023
-            </div>
-            <!-- <q-separator
+        <div class="bg-blue-10 text-h6 text-bold text-center text-white">
+          WElCOME PAGE
+        </div>
+        <div class="text-h6 text-bold text-center text-white">
+          Interactive Habitat Map<br />Around Hong Kong Rocky Shore<br />by ECF
+          2019-2023
+        </div>
+        <!-- <q-separator
               dark
               inset
             /> -->
-            <div class="q-ma-sm">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-                quis tempore praesentium illum? Molestiae ipsa repellendus
-                ipsam! Quaerat placeat eveniet, quo tempora dolorum, maxime eos
-                odit, voluptate aspernatur assumenda ipsa.
-              </p>
-            </div>
-
-            <div class="row justify-center">
-              <q-btn
-                no-caps
-                color="blue-10"
-                size="15px"
-                push
-                style="width: 280px"
-              >
-                <div class="row items-center no-wrap">
-                  <q-icon
-                    center
-                    name="zoom_in"
-                  />
-                  <div class="text-center no-caps">
-                    Please Select Any Zone<br />for Localized Data
-                  </div>
-                </div>
-              </q-btn>
-            </div>
-          </q-card>
+        <div class="q-ma-sm">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+            quis tempore praesentium illum? Molestiae ipsa repellendus ipsam!
+            Quaerat placeat eveniet, quo tempora dolorum, maxime eos odit,
+            voluptate aspernatur assumenda ipsa.
+          </p>
         </div>
 
-        <div
-          class="col-8 bg-grey-6"
-          style="overflow: auto"
+        <div class="row justify-center">
+          <q-btn no-caps color="blue-10" size="15px" push style="width: 280px">
+            <div class="row items-center no-wrap">
+              <q-icon center name="zoom_in" />
+              <div class="text-center no-caps">
+                Please Select Any Zone<br />for Localized Data
+              </div>
+            </div>
+          </q-btn>
+        </div>
+      </q-card>
+    </div>
+
+    <div class="col-8 bg-grey-6" style="overflow: auto">
+      <div class="map">
+        <l-map
+          style="height: 480px; width: 100%"
+          :zoom="zoom"
+          :center="center"
+          :options="mapOptions"
+          :bounds="bounds"
+          :max-bounds="maxBounds"
+          :max-zoom="maxZoom"
+          :min-zoom="minZoom"
+          ref="myMapRef"
         >
-          <div class="map">
-            <l-map
-              style="height: 480px; width: 100%"
-              :zoom="zoom"
-              :center="center"
-              :options="mapOptions"
-              :bounds="bounds"
-              :max-bounds="maxBounds"
-              :max-zoom="maxZoom"
-              :min-zoom="minZoom"
-              ref="myMapRef"
-            >
-              <l-tile-layer
-                :url="url"
-                :attribution="attribution"
-              />
-              <l-geo-json
-                :geojson="geojson"
-                :options="geoJSONOptions"
-              >
-              </l-geo-json>
-              
-              <l-geo-json
-                :geojson="SitesLocation"
-              >
-              </l-geo-json>
+          <l-tile-layer :url="url" :attribution="attribution" />
+          <l-geo-json :geojson="geojson" :options="geoJSONOptions">
+          </l-geo-json>
 
-            </l-map>
-          </div>
-        </div>
+          <l-geo-json :geojson="SitesLocation"> </l-geo-json>
+        </l-map>
       </div>
     </div>
-    <div class="col-6"></div>
   </div>
 </template>
 
