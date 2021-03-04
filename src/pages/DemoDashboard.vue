8e93a39dbca537d05501edff2e3f39ff377db62a
Yvonne <liyiwan2020@gmail.com>
2021-03-04 20:59:16 +0800

    modify dashboard layout

--- src/pages/DemoDashboard.vue
+++ src/pages/DemoDashboard.vue
@@ -1,39 +1,89 @@
 <template>
-  <div class="row q-pa-sm justify-center">
-    <q-card class="col-3">
-      <div class="text-h5 text-bold row justify-center">Temperature</div>
-      <highcharts :options="chartOptions"></highcharts>
-      <highcharts :options="chartOptions"></highcharts>
-    </q-card>
-    <q-card class="col-6">
-      <div class="text-h5 text-bold row justify-center">Site Name</div>
-     <div class="col-3 ">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-          quis tempore praesentium illum? Molestiae ipsa repellendus
-          ipsam! Quaerat placeat eveniet, quo tempora dolorum, maxime eos
-          odit, voluptate aspernatur assumenda ipsa.
-        </p>
-     </div>
-      <div class="col-3 ">
-        <img src="~assets/placeholder.png" />
-      </div>
-    </q-card>
-    <q-card class="col-3">
-      <div class="text-h5 text-bold row justify-center">Wave Fetch</div>
-    </q-card>
+  <div>
+    <div class="row q-pa-sm justify-center">
+      <q-card class="col-3">
+        <div class="text-h5 text-bold row justify-center">Temperature</div>
+        <highcharts :options="chartOptions"></highcharts>
+        <highcharts :options="chartOptions"></highcharts>
+      </q-card>
+      <q-card class="col-6 q-pa-sm">
+
+        <div class="q-col-gutter-sm row">
+          <q-card-section
+            vertical
+            class="col-6"
+          >
+            <div class="text-h5 text-bold row justify-center">Site Name</div>
+            <q-card-section>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+              quis tempore praesentium illum? Molestiae ipsa repellendus
+              ipsam! Quaerat placeat eveniet, quo tempora dolorum, maxime eos
+              odit, voluptate aspernatur assumenda ipsa.
+              <q-img
+                :ratio="16/9"
+                src="~assets/placeholder.png"
+              />
+            </q-card-section>
+
+          </q-card-section>
+
+          <q-card-section class="col-6">
+            <l-map
+              :zoom="zoom"
+              :center="center"
+            >
+              <l-tile-layer
+                :url="url"
+                :attribution="attribution"
+              />
+            </l-map>
+          </q-card-section>
+
+        </div>
+      </q-card>
+      <q-card class="col-3">
+        <div class="text-h5 text-bold row justify-center">Wave Fetch</div>
+        <highcharts :options="chartOptions"></highcharts>
+        <highcharts :options="chartOptions"></highcharts>
+      </q-card>
+    </div>
+    <div class="row q-pa-sm justify-center">
+      <q-card class="col-12">
+        <div class="text-h5 text-bold row justify-center">Nutrient Levels</div>
+        <q-card-section
+          horizontal
+          class="col-6"
+        >
+          <highcharts :options="chartOptions"></highcharts>
+        </q-card-section>
+        <q-card-section
+          horizontal
+          class="col-6"
+        >
+          <highcharts :options="chartOptions"></highcharts>
+        </q-card-section>
+      </q-card>
+    </div>
   </div>
 
 </template>
 
 <script>
+import { LMap, LTileLayer } from "vue2-leaflet";
 import Vue from "vue";
 import HighchartsVue from "highcharts-vue";
 Vue.use(HighchartsVue);
 
 export default {
+  components: { LMap, LTileLayer },
   data() {
     return {
+      center: L.latLng(22.3908, 114.1721),
+      zoom: 10.25,
+      url:
+        "http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+      attribution: "Source &copy; Esri",
+
       chartOptions: {
         chart: {
           type: "spline",
