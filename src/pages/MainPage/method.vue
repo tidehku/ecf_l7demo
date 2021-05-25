07ee7037e1e6a92364d8b75946a9591f87346b4f
Vicellken <gulivecassie@gmail.com>
2021-05-25 19:07:39 +0800

    update method section style

--- src/pages/MainPage/method.vue
+++ src/pages/MainPage/method.vue
@@ -5,14 +5,14 @@
         <div class="text-indigo title">
           Stratification of Hong Kong coastline
         </div>
-        <div class="text-subtitle1"> {{ Stratification }}</div>
+        <div class="text-subtitle1">{{ Stratification }}</div>
       </div>
       <div class="q-pa-md col-5">
-        <q-img
-          class="col-5"
-          src="~/assets/method1.png"
-        />
-        <div class="q-px-md bg-black text-white">Map showing demarcation of regions and location of sampling sites within each region.</div>
+        <q-img class="col-5" src="~/assets/method1.png" />
+        <div class="q-px-md bg-black text-white">
+          Map showing demarcation of regions and location of sampling sites
+          within each region.
+        </div>
       </div>
     </div>
 
@@ -21,24 +21,32 @@
         Rocky shore assemblage surveys using transect and quadrat methods
       </div>
       <div class="row justify-evenly">
-        <div
-          class="justify-center"
-          style="width:28%"
-        >
-          <q-img src="~/assets/method2.png" />
-          <div class="q-px-md bg-black text-white">Morning survey in Shek O ....</div>
-          <q-img src="~/assets/method3.png" />
-          <div class="q-px-md bg-black text-white">The ECF team surveying rocky shores in Luk Keng</div>
-          <q-img src="~/assets/method4.png" />
-          <div class="q-px-md bg-black text-white">The ECF team surveying rocky shores in Tai O</div>
+        <div class="justify-center" style="width:28%">
+          <q-img src="~/assets/method2.png">
+            <div class="absolute-bottom text-subtitle1 text-center">
+              Morning survey in Shek O ....
+            </div>
+          </q-img>
+          <q-img src="~/assets/method3.png">
+            <div class="absolute-bottom text-subtitle1 text-center">
+              The ECF team surveying rocky shores in Luk Keng
+            </div>
+          </q-img>
+          <q-img src="~/assets/method4.png">
+            <div class="absolute-bottom text-subtitle1 text-center">
+              The ECF team surveying rocky shores in Tai O
+            </div>
+          </q-img>
         </div>
-        <div
-          class="q-px-lg"
-          style="width:60%"
-        >
-          <div class="text-subtitle1"> {{ method1 }}</div>
-          <br /> <br />
-          <div class="text-subtitle1"> {{ method2 }}</div>
+        <div class="q-px-lg" style="width:60%">
+          <div class="text-subtitle1" style=" font-size: 1.25rem">
+            {{ method1 }}
+          </div>
+          <br />
+          <br />
+          <div class="text-subtitle1" style=" font-size: 1.25rem">
+            {{ method2 }}
+          </div>
         </div>
       </div>
     </div>
@@ -48,21 +56,17 @@
         Quantifying habitat properties
       </div>
       <div class="row justify-evenly">
-
         <div class="col-5">
-          <div class="q-px-lg text-subtitle1"> {{ quantifying }}</div>
+          <div class="q-px-lg text-subtitle1">{{ quantifying }}</div>
         </div>
         <div class="col-6">
-          <q-img
-            style="width:50%"
-            src="~/assets/method5.png"
-          />
-          <q-img
-            style="width:50%"
-            src="~/assets/method6.png"
-          />
-          <div class="q-px-md bg-black text-white">Custom-built wavelogger (left image) and rugged temperature loggers (right image) deployed on shores to record wave force and rock surface temperature. </div>
-
+          <q-img style="width:50%" src="~/assets/method5.png" />
+          <q-img style="width:50%" src="~/assets/method6.png" />
+          <div class="q-px-md bg-black text-white">
+            Custom-built wavelogger (left image) and rugged temperature loggers
+            (right image) deployed on shores to record wave force and rock
+            surface temperature.
+          </div>
         </div>
       </div>
     </div>
@@ -70,7 +74,8 @@
     <div class="q-pa-md bg-indigo">
       <div class="title text-center text-white">Glossary</div>
       <div class="text-subtitle1 text-center text-white">
-        This glossary outlines the definitions of the biological and physical terms/measurements adopted in this survey
+        This glossary outlines the definitions of the biological and physical
+        terms/measurements adopted in this survey
       </div>
     </div>
 
@@ -90,7 +95,7 @@
         <template v-slot:body-cell-name="props">
           <q-td :props="props">
             <div>
-              {{props.value}}
+              {{ props.value }}
             </div>
           </q-td>
 
@@ -116,7 +121,7 @@
         <template v-slot:body-cell-name="props">
           <q-td :props="props">
             <div>
-              {{props.value}}
+              {{ props.value }}
             </div>
           </q-td>
 
@@ -145,86 +150,82 @@ export default {
           required: true,
           label: "Terminology",
           align: "left",
-          field: (row) => row.name,
-          format: (val) => `${val}`,
+          field: row => row.name,
+          format: val => `${val}`
         },
         {
           name: "description",
           align: "left",
-          label: "Description",
-        },
+          label: "Description"
+        }
       ],
 
       bioGlossary: [
         {
           name: "Mobile species",
-          description: `Species that are able to move around on their own, including most gastropods (snails and limpets), crabs and fishes.`,
+          description: `Species that are able to move around on their own, including most gastropods (snails and limpets), crabs and fishes.`
         },
         {
           name: "Sessile species",
-          description: `Species with limited or no ability to move around, such as algae, bivalves (oysters and mussels) and some sea anemones.  In some cases, an entity in sessile species might not correspond to a single species but a mix of microbial species constituting an unique functional entity, e.g. high shore biofilm, that is hard to be further separated into species level in the field.`,
-        },
-        {
-          name: "Mobile species",
-          description: `Species that are able to move around on their own, including most gastropods (snails and limpets), crabs and fishes.`,
+          description: `Species with limited or no ability to move around, such as algae, bivalves (oysters and mussels) and some sea anemones.  In some cases, an entity in sessile species might not correspond to a single species but a mix of microbial species constituting an unique functional entity, e.g. high shore biofilm, that is hard to be further separated into species level in the field.`
         },
         {
           name: "Shannon’s diversity index (H’) ",
-          description: `A diversity index based on Shannon & Weaver (1949), calculated using the formula H’ = - Σ pi log pi (where i ranges from 1 to the total number of species found, and pi is the proportion of the abundance of species i in the sampling area).  A higher value of H’ indicates a more diverse assemblage at a sampling area (either by increasing the number of species or having species with more similar numbers of individuals).  In our project, H’ is calculated based on identified mobile species where count is available.`,
+          description: `A diversity index based on Shannon & Weaver (1949), calculated using the formula H’ = - Σ pi log pi (where i ranges from 1 to the total number of species found, and pi is the proportion of the abundance of species i in the sampling area).  A higher value of H’ indicates a more diverse assemblage at a sampling area (either by increasing the number of species or having species with more similar numbers of individuals).  In our project, H’ is calculated based on identified mobile species where count is available.`
         },
         {
           name: "Species richness (S)",
-          description: `The total number of either count or mobile species found in a sampling area, where counts or presence/absence data are available (either based on region/site in this GIS).`,
+          description: `The total number of either count or mobile species found in a sampling area, where counts or presence/absence data are available (either based on region/site in this GIS).`
         },
         {
           name: "Pielou’s evenness index (J)",
-          description: `An index used to indicate the evenness of species distribution in an assemblage, calculated by dividing H’ with the maximum possible value of H’.  A higher J indicates a more even assemblage at a sampling area (species are having more similar number of individuals, instead of having heavily dominant species).  In our project, J is calculated based on identified mobile species where count is available.`,
+          description: `An index used to indicate the evenness of species distribution in an assemblage, calculated by dividing H’ with the maximum possible value of H’.  A higher J indicates a more even assemblage at a sampling area (species are having more similar number of individuals, instead of having heavily dominant species).  In our project, J is calculated based on identified mobile species where count is available.`
         },
         {
           name: "Chlorophyll a (Chl a) concentration",
-          description: `A proxy for algal/biofilm biomass on the rock surface, measured using spectrophotometry (extraction using cold methanol) and expressed in µg/cm².  A higher value indicates higher standing stock of algae/cyanobacteria on the rock surface, which could be resulted from a higher productivity in the surrounding pelagic environment, or reduced consumer pressure (less abundance of grazers on the shore, see Williams et al. 2000).`,
+          description: `A proxy for algal/biofilm biomass on the rock surface, measured using spectrophotometry (extraction using cold methanol) and expressed in µg/cm².  A higher value indicates higher standing stock of algae/cyanobacteria on the rock surface, which could be resulted from a higher productivity in the surrounding pelagic environment, or reduced consumer pressure (less abundance of grazers on the shore, see Williams et al. 2000).`
         },
         {
           name: "Organic matter (OM) concentration",
-          description: `A proxy for total biomass (including algae, cyanobacteria, bacteria etc) on the rock surface, measured using mass loss-on-ignition (in a muffle furnace at 500 °C for 4 h) and expressed in mg/cm². `,
-        },
+          description: `A proxy for total biomass (including algae, cyanobacteria, bacteria etc) on the rock surface, measured using mass loss-on-ignition (in a muffle furnace at 500 °C for 4 h) and expressed in mg/cm². `
+        }
       ],
       phyGlossary: [
         {
           name: "Total wave fetch",
-          description: `The total distance of seas along 16 equiangular directions from a coastal location to surrounding land masses (up to 200 km).  Longer the wave fetch generally implies stronger wave action impacting the shore (see Burrows et al. 2008).`,
+          description: `The total distance of seas along 16 equiangular directions from a coastal location to surrounding land masses (up to 200 km).  Longer the wave fetch generally implies stronger wave action impacting the shore (see Burrows et al. 2008).`
         },
         {
           name: "Max. wave force",
-          description: `The maximum force (measured in N) estimated using a custom-built wavelogger with an accelerometer.  The logger was deployed at the low-mid level on the shore for 48 h, recording acceleration every 10 s.  The readings were, therefore, subjected to the prevailing wind direction and speed during the recording period.`,
+          description: `The maximum force (measured in N) estimated using a custom-built wavelogger with an accelerometer.  The logger was deployed at the low-mid level on the shore for 48 h, recording acceleration every 10 s.  The readings were, therefore, subjected to the prevailing wind direction and speed during the recording period.`
         },
         {
           name: "Rock temperature",
-          description: `Rock surface temperature (measured in °C) using rugged temperature loggers attached onto the rock surface using marine epoxy.  Temperatures were recorded every 1 h for a period of 2 years at our study sites.`,
+          description: `Rock surface temperature (measured in °C) using rugged temperature loggers attached onto the rock surface using marine epoxy.  Temperatures were recorded every 1 h for a period of 2 years at our study sites.`
         },
         {
           name: "Shore aspect",
-          description: `The direction faced by the shore, measured using a hand-held compass (expressed in ° from the north direction).`,
+          description: `The direction faced by the shore, measured using a hand-held compass (expressed in ° from the north direction).`
         },
         {
           name: "Shore slope",
-          description: `The inclination of the shore measured in °, ranging from 0° (a completely horizontal shore) to 90° (a completely vertical shore).`,
+          description: `The inclination of the shore measured in °, ranging from 0° (a completely horizontal shore) to 90° (a completely vertical shore).`
         },
         {
           name: "Rock type",
-          description: `Geological formation/composition of rocks in our sampling sites, adopting the classification based on CEDD (2006).`,
+          description: `Geological formation/composition of rocks in our sampling sites, adopting the classification based on CEDD (2006).`
         },
         {
           name: "Chart Datum (C. D.)",
-          description: `Water level during the lowest astronomical tide, which has been adopted as the reference level to measure tidal height in Hong Kong since 1917 (Lands Department, 1995).`,
+          description: `Water level during the lowest astronomical tide, which has been adopted as the reference level to measure tidal height in Hong Kong since 1917 (Lands Department, 1995).`
         },
         {
           name: "Wave exposure icons",
-          description: `In the regional landing page where you can select information of the four study sites within that region, there is a “wave exposure icon” next to each site.  As we deliberately selected four sites with different wave exposures within a region, the icon indicates the relative wave exposure of that site: Most exposed within the region: Intermediately exposed within the region:Least exposed within the region: `,
-        },
-      ],
+          description: `In the regional landing page where you can select information of the four study sites within that region, there is a “wave exposure icon” next to each site.  As we deliberately selected four sites with different wave exposures within a region, the icon indicates the relative wave exposure of that site: Most exposed within the region: Intermediately exposed within the region:Least exposed within the region: `
+        }
+      ]
     };
-  },
+  }
 };
 </script>
 
@@ -256,7 +257,7 @@ export default {
   background-color: $teal-1
   font-weight: bold
 .description
-  font-size: 1 em
+  font-size: 1em
   white-space: normal
   margin-top: 4px
 </style>
