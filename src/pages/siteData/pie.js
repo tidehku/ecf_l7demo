1e170950f7a9dcea145582f2b4e454fe5bd3a647
Yifei Gu <guyf0601@connect.hku.hk>
2021-07-18 19:27:58 +0800

    visualisation rules; spell check

--- src/pages/siteData/pie.js
+++ src/pages/siteData/pie.js
@@ -28,7 +28,7 @@ export const pieData = {
     },
     series: [
       {
-        name: "Species Abundance",
+        name: "Species abundance",
         colorByPoint: true,
         colors: ["#719DC8", "#79C6D1", "#a0c2a0", "#ffdc73", "#e8a969"],
         showInLegend: false,
@@ -68,7 +68,7 @@ export const pieData = {
     },
     series: [
       {
-        name: "Species Abundance",
+        name: "Incidence proportion",
         colorByPoint: true,
         colors: ["#719DC8", "#79C6D1", "#a0c2a0", "#ffdc73", "#e8a969"],
         showInLegend: false,
@@ -80,6 +80,43 @@ export const pieData = {
     chart: {
       type: "pie",
       height: (1 / 1) * 100 + "%"
+    },
+    credits: {
+      enabled: false
+    },
+    title: {
+      text: ""
+    },
+    subtitle: {
+      text: ""
+    },
+    legend: {
+      enabled: true
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: "pointer",
+        dataLabels: {
+          enabled: false
+        },
+        showInLegend: false
+      }
+    },
+    series: [
+      {
+        name: "Species abundance",
+        colorByPoint: true,
+        colors: ["#719DC8", "#79C6D1", "#a0c2a0", "#ffdc73", "#e8a969"],
+        showInLegend: false,
+        data: []
+      }
+    ]
+  },
+  TOPie2: {
+    chart: {
+      type: "pie",
+      height: (1 / 1) * 100 + "%" // 3:4 ratio
     },
     credits: {
       enabled: false
@@ -108,44 +145,7 @@ export const pieData = {
     },
     series: [
       {
-        name: "Species Abundance",
-        colorByPoint: true,
-        colors: ["#719DC8", "#79C6D1", "#a0c2a0", "#ffdc73", "#e8a969"],
-        showInLegend: false,
-        data: []
-      }
-    ]
-  },
-  TOPie2: {
-    chart: {
-      type: "pie",
-      height: (1 / 1) * 100 + "%" // 3:4 ratio
-    },
-    credits: {
-      enabled: false
-    },
-    title: {
-      text: ""
-    },
-    subtitle: {
-      text: ""
-    },
-    legend: {
-      enabled: true
-    },
-    plotOptions: {
-      pie: {
-        allowPointSelect: true,
-        cursor: "pointer",
-        dataLabels: {
-          enabled: false
-        },
-        showInLegend: false
-      }
-    },
-    series: [
-      {
-        name: "Species Abundance",
+        name: "Incidence proportion",
         colorByPoint: true,
         colors: ["#719DC8", "#79C6D1", "#a0c2a0", "#ffdc73", "#e8a969"],
         showInLegend: false,
