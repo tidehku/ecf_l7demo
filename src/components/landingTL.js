58911f3c3dbf135f0e7305d48300472adb0df875
Yifei Gu <guyf0601@connect.hku.hk>
2021-07-22 17:27:35 +0800

    add TL summer data

---
+++ src/components/landingTL.js
@@ -0,0 +1,40 @@
+const landingTL = {
+  winter2020: {
+    highShoreTemp: 35.67,
+    highShoreTempDelta: 6.11,
+    highShoreTempN: 3,
+    lowShoreTemp: 33.88,
+    lowShoreTempDelta: 4.68,
+    lowShoreTempN: 4,
+    chla: 11.6,
+    chlaDelta: 10.38,
+    chlaN: 3,
+    om: 14.43,
+    omDelta: 8.41,
+    omN: 4,
+    mobileMax: 22,
+    mobileMin: 17,
+    sessileMax: 15,
+    sessileMin: 13
+  },
+  summer2021: {
+    highShoreTemp: 51.7,
+    highShoreTempDelta: 2.4,
+    highShoreTempN: 4,
+    lowShoreTemp: 45.9,
+    lowShoreTempDelta: 2.6,
+    lowShoreTempN: 4,
+    chla: 12.40,
+    chlaDelta: 6.01,
+    chlaN: 4,
+    om: 0.02,
+    omDelta: 0.01,
+    omN: 4,
+    mobileMax: "NA",
+    mobileMin: "NA",
+    sessileMax: "NA",
+    sessileMin: "NA"
+  }
+};
+
+export default landingTL;
--- src/pages/iH/interactiveHabitat_TL.vue
+++ src/pages/iH/interactiveHabitat_TL.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page class="q-pa-xs row justify-center bg-grey-5 no-wrap">
+  <q-page class="q-pa-xs row justify-center no-wrap">
     <q-card class="q-ma-sm" style="width: 33%; height:81vh" bordered>
       <q-toolbar class="bg-indigo-6 text-h5 text-bold text-white">
         Tolo Harbour region
@@ -15,7 +15,7 @@
           </q-route-tab>
         </q-tabs>
       </q-toolbar>
-      <div class="q-pa-sm">{{ regionTL }}</div>
+      <div class="q-pa-sm" style="font-size: 1.1rem">{{ regionTL }}</div>
 
       <l-map
         class="map"
@@ -40,77 +40,8 @@
     </q-card>
 
     <div style="width: 66.3%">
-      <q-card class="bg-indigo-1" style="height: 40vh">
-        <div
-          class="q-px-md bg-indigo-6 text-center text-h6 text-bold text-white rounded-borders"
-        >
-          Regional average data summary
-        </div>
-        <div class="row q-py-xs justify-evenly no-wrap">
-          <q-card class="q-pa-sm" style="width: 22%; min-height: 32vh">
-            <div class="q-px-sm">
-              <y>Max. rock temperature</y> <br />
-              High shore: <br />
-              <h>35.67</h> <b> ± 6.11 ℃</b>
-              <div class="row justify-end">
-                (n = 3)
-              </div>
-              Mid shore: <br />
-              <h>33.88</h> <b> ± 4.68 ℃</b>
-              <div class="row justify-end">
-                (n = 4)
-              </div>
-            </div>
-          </q-card>
-          <q-card class="q-pa-sm" style="width: 18%">
-            <div class="q-px-sm">
-              <y>Total wave fetch</y> <br />
-              <div class="items-end">
-                Max: <br />
-                <h>17.98</h> <b> km</b> <br /><br />
-                Min: <br />
-                <h>12.31</h> <b> km</b>
-                <div class="row justify-end">
-                  (n = 2)
-                </div>
-              </div>
-            </div>
-          </q-card>
-          <q-card class="q-pa-sm" style="width: 24%">
-            <div class="q-px-sm">
-              <y>Nutrient levels</y> <br />
-              Chlorophyll <i>a</i>: <br />
-              <h>11.60</h> <b> ± 10.38 µg/cm<sup>2</sup></b>
-              <div class="row justify-end">
-                (n = 4)
-              </div>
-              Organic matter: <br />
-              <h>14.43</h> <b> ± 8.41 mg/cm<sup>2</sup></b>
-              <div class="row justify-end">
-                (n = 4)
-              </div>
-            </div>
-          </q-card>
-          <q-card class="q-pa-sm" style="width: 16%">
-            <div class="q-px-sm">
-              <y>Mobile species richness</y> <br /><br />
-              <div class="items-center">
-                Max: <h>22</h> <br /><br />
-                Min: <h>17</h>
-              </div>
-            </div>
-          </q-card>
-          <q-card class="q-pa-sm" style="width: 16%">
-            <div class="q-px-sm">
-              <y>Sessile species richness</y> <br /><br />
-              <div class="items-center">
-                Max: <h>15</h> <br /><br />
-                Min: <h>13</h>
-              </div>
-            </div>
-          </q-card>
-        </div>
-      </q-card>
+      <landingTL />
+
       <div class="row no-wrap justify-around">
         <q-card class="q-ma-md" style="width: 24%; height: 40vh">
           <div class="q-pa-sm bg-indigo-6 text-h5 text-bold text-white">
@@ -459,14 +390,16 @@
 <script>
 import { LMap, LGeoJson, LControlScale } from "vue2-leaflet";
 import "leaflet.zoomhome/dist/leaflet.zoomhome.js";
+import landingTL from "../../components/landingTL.vue";
 
 export default {
-  name: "EA",
+  name: "TL",
   components: {
     LMap,
     LGeoJson,
     LControlScale,
-    LTileLayer: () => import("components/tileLayer")
+    LTileLayer: () => import("components/tileLayer"),
+    landingTL
   },
   data() {
     return {
@@ -535,13 +468,6 @@ export default {
   display: flex
   flex-direction: column
 
-h
-  color: $red-8
-  font-size: 24px
-
-y
-  font-weight: 600
-  color: $indigo-4
 .map
   flex: 1
   border: 5px solid white
